d49d9371852f7f02a3a60fb8a6f75491b0baa350
MikeVezina <vezina95@bell.net>
2015-09-21 18:31:31 -0400

    - Added name, homeAddress, and phoneNumber fields - Added Getter/Setter methods for each of the fields - Constructor now initializes these fields

--- src/buddyinfo/BuddyInfo.java
+++ src/buddyinfo/BuddyInfo.java
@@ -2,10 +2,48 @@ package buddyinfo;
 
 public class BuddyInfo
 {
-	public BuddyInfo()
+	private String name;
+	private String homeAddress;
+	private String phoneNumber;
+	
+	public BuddyInfo(String name, String address, String phoneNumber)
 	{
-		
+		this.name = name;
+		this.setHomeAddress(address);
+		this.setPhoneNumber(phoneNumber);
 	}
 
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void setName(String newName)
+	{
+		this.name = newName;
+	}
+
+	public String getPhoneNumber()
+	{
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber)
+	{
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getHomeAddress()
+	{
+		return homeAddress;
+	}
+
+	public void setHomeAddress(String homeAddress)
+	{
+		this.homeAddress = homeAddress;
+	}
+	
+	
+	
 
 }
