f31e076a0ba8e034351ae5e1eaf96a787090ca20
MikeVezina <vezina95@bell.net>
2015-09-21 18:28:22 -0400

    - Added the ability to add or remove buddy info from the address book - Added a main method in the AddressBook class

--- src/buddyinfo/AddressBook.java
+++ src/buddyinfo/AddressBook.java
@@ -5,8 +5,31 @@ import java.util.*;
 
 public class AddressBook
 {
-	public AddressBook()
-	{
+		private List<BuddyInfo> buddyInfo;
 		
-	}
+		public AddressBook()
+		{
+			this.buddyInfo = new ArrayList<>();
+		}
+		
+		public void addBuddy(BuddyInfo bI)
+		{
+			if(bI != null)
+				this.buddyInfo.add(bI);
+		}
+		
+		public BuddyInfo removeBuddy(int index)
+		{
+			if(index >= 0 && index < this.buddyInfo.size())
+				return this.buddyInfo.remove(index);
+			
+			return null;
+		}
+		
+		
+		public static void main(String[] args)
+		{
+			BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", 1234);
+		}
+	
 }
