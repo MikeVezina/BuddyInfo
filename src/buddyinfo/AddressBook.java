6ff0a3a07b5c5f9d44cf769332694e0c3d383a26
MikeVezina <vezina95@bell.net>
2015-11-19 12:42:52 -0500

    Added Ability To Import And Export Address Books
    - Factory Method for creating buddies (In BuddyInfo)

--- src/buddyinfo/AddressBook.java
+++ src/buddyinfo/AddressBook.java
@@ -1,185 +1,82 @@
 package buddyinfo;
 
-import java.awt.CardLayout;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOError;
 import java.io.IOException;
-import java.util.*;
 
 import javax.swing.*;
 
-public class AddressBook extends JFrame implements ActionListener
+
+public class AddressBook
 {
 
-	// GUI FUNCTIONS
-
-	private List<BuddyInfo>	buddyInfoList;
-	private AddressBook		addressBook;
-
-	private JMenuItem		createAddressBookButton;
-	private JMenuItem		saveAddressBookButton;
-	private JMenuItem		displayAddressBookButton;
-	private JMenuItem		addBuddyButton;
-
-	private JMenuBar		mainMenuBar;
-
-	private JMenu			addressBookMenu;
-	private JMenu			buddyInfoMenu;
-
-	private JTextArea		textArea;
-
+	public final static String FILE_NAME = "addressbook.txt";
+	private DefaultListModel<BuddyInfo>	buddyInfoListModel;
+	
 	public AddressBook()
 	{
-		super("Address Book Manager");
-
-		this.setSize(new Dimension(500, 500));
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.getContentPane().setLayout(new FlowLayout());
-
-		InitializeComponents();
+		buddyInfoListModel = new DefaultListModel<>();
 	}
-
-	private void InitializeComponents()
-	{
-		mainMenuBar = new JMenuBar();
-
-		addressBookMenu = new JMenu("Address Book");
-		buddyInfoMenu = new JMenu("Buddy Info");
-
-		createAddressBookButton = new JMenuItem("Create");
-		createAddressBookButton.setSize(10, 10);
-		createAddressBookButton.addActionListener(this);
-		addressBookMenu.add(createAddressBookButton);
-
-		saveAddressBookButton = new JMenuItem("Save");
-		saveAddressBookButton.setSize(10, 10);
-		saveAddressBookButton.addActionListener(this);
-		saveAddressBookButton.setEnabled(false);
-		addressBookMenu.add(saveAddressBookButton);
-
-		displayAddressBookButton = new JMenuItem("Display All Buddy Information");
-		displayAddressBookButton.setSize(10, 10);
-		displayAddressBookButton.addActionListener(this);
-		displayAddressBookButton.setEnabled(false);
-		addressBookMenu.add(displayAddressBookButton);
-
-		addBuddyButton = new JMenuItem("Add Buddy");
-		addBuddyButton.setSize(10, 10);
-		addBuddyButton.addActionListener(this);
-		addBuddyButton.setEnabled(false);
-		buddyInfoMenu.add(addBuddyButton);
-
-		mainMenuBar.add(addressBookMenu);
-		mainMenuBar.add(buddyInfoMenu);
-
-		this.add(mainMenuBar);
-
-		textArea = new JTextArea();
-
-		this.add(textArea);
-
-		textArea.setPreferredSize(new Dimension(500, 250));
-
-		this.setVisible(true);
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e)
-	{
-		if (e.getSource().equals(createAddressBookButton))
-		{
-			CreateAddressBook();
-			
-			saveAddressBookButton.setEnabled(true);
-			displayAddressBookButton.setEnabled(true);
-			addBuddyButton.setEnabled(true);
-		}
-		else if (e.getSource().equals(saveAddressBookButton))
-		{
-			try
-			{
-				SaveAddressBook();
-			}
-			catch (Exception exc)
-			{
-
-			}
-		}
-		else if (e.getSource().equals(displayAddressBookButton))
-		{
-			DisplayAddressBook();
-		}
-		else if (e.getSource().equals(addBuddyButton))
-		{
-			String buddyName = (String) JOptionPane.showInputDialog("Please Enter the name of the buddy:");
-			String homeAddress = (String) JOptionPane.showInputDialog("Please Enter the Home Address of the buddy:");
-			String phoneNumber = (String) JOptionPane.showInputDialog("Please Enter the Phone Number of the buddy:");
-			buddyInfoList.add(new BuddyInfo(buddyName, homeAddress, phoneNumber));
-			textArea.append("Created BuddyInfo: " + buddyName + "\n");
-		}
-	}
-
-	private void CreateAddressBook()
-	{
-		this.buddyInfoList = new ArrayList<>();
-		
-		textArea.setText(textArea.getText() + "Created Address Book\n");
-
-
-	}
-
-	private void SaveAddressBook() throws IOException
-	{
-		FileWriter fW = new FileWriter("/Users/MVezina/Desktop/AddrBook.txt");
-
-		for (BuddyInfo bI : buddyInfoList)
-		{
-			fW.write(bI.toString());
-		}
-
-		fW.close();
-		textArea.append("Saved Address Book\n");
-	}
-
-	private void DisplayAddressBook()
-	{
-		textArea.setText("");
-		textArea.append("Address Book Contents:\n\n");
-		if(buddyInfoList.size() == 0)
-		{
-			textArea.append("No Buddies Found!\n");
-		}
-		
-		for (BuddyInfo bI : buddyInfoList)
-		{
-			textArea.append(bI.toString() + "\n\n");
-		}
-	}
-
-	// END GUI FUNCTIONS
 
 	public void addBuddy(BuddyInfo bI)
 	{
-		if (bI != null)
-			this.buddyInfoList.add(bI);
+		if(buddyInfoListModel.contains(bI))
+			return;
+		
+		buddyInfoListModel.addElement(bI);
 	}
-
-	public BuddyInfo removeBuddy(int index)
+	
+	
+	public void exportAddressBook() throws IOException
 	{
-		if (index >= 0 && index < this.buddyInfoList.size())
-			return this.buddyInfoList.remove(index);
-
-		return null;
+		BufferedWriter bW = new BufferedWriter(new FileWriter(FILE_NAME));
+		
+		for(int i = 0; i < buddyInfoListModel.size(); i++)
+		{
+			bW.write(buddyInfoListModel.getElementAt(i) + "\n");
+			
+		}
+		
+		bW.close();
 	}
 
-	public static void main(String[] args)
+	public void importAddressBook() throws IOException
 	{
-		new AddressBook();
-
+		BufferedReader bR = new BufferedReader(new FileReader(FILE_NAME));
+		while(bR.ready())
+		{
+			BuddyInfo newBuddy = BuddyInfo.importBuddy(bR.readLine());
+			
+			if(newBuddy != null)
+			this.addBuddy(newBuddy);
+			
+		}
+		
+		bR.close();
 	}
+	
+	public void removeBuddy(int index)
+	{
+		buddyInfoListModel.removeElementAt(index);
+	}
+
+	public DefaultListModel<BuddyInfo> getListModel()
+	{
+		return buddyInfoListModel;
+	}
+	
+	public int size()
+	{
+		return buddyInfoListModel.size();
+	}
+	
+	public void clear()
+	{
+		buddyInfoListModel.clear();
+	}
+	
+	
 
 }
--- src/buddyinfo/BuddyInfo.java
+++ src/buddyinfo/BuddyInfo.java
@@ -1,25 +1,98 @@
 package buddyinfo;
 
+
 public class BuddyInfo
 {
-	private String name;
-	private String homeAddress;
-	private String phoneNumber;
+	private String	name;
+	private String	homeAddress;
+	private String	phoneNumber;
+	private String greeting;
+	private int buddyAge;
 	
-	public BuddyInfo(String name, String address, String phoneNumber)
+	private final static String DELIMITER = "$";
+
+	
+
+	public BuddyInfo(BuddyInfo buddyInfo)
 	{
-		this.name = name;
-		this.setHomeAddress(address);
-		this.setPhoneNumber(phoneNumber);
+		this.name = buddyInfo.name;
+		this.homeAddress = buddyInfo.homeAddress;
+		this.phoneNumber = buddyInfo.phoneNumber;
+		this.greeting = buddyInfo.greeting;
+		this.buddyAge = buddyInfo.buddyAge;
+	}
+	
+	public int getBuddyAge()
+	{
+		return buddyAge;
+	}
+	
+	public boolean isOver18()
+	{
+		return (buddyAge > 18);
 	}
 
+	public void setBuddyAge(int buddyAge)
+	{
+		if(buddyAge < 0)
+			return;
+		this.buddyAge = buddyAge;
+	}
+
+	public BuddyInfo(String name, String address, String phoneNumber, String greeting, int age)
+	{
+		// Ensure name is not null
+		if(name == null)
+			this.name = "";
+		else
+			this.name = name;
+		
+		// Ensure address is not null
+		if (address == null)
+			this.homeAddress = "";
+		else
+			this.homeAddress = address;
+		
+		// Ensure phone number is not null
+		if (phoneNumber == null)
+			this.phoneNumber = "";
+		else
+			this.phoneNumber = phoneNumber;
+		
+		if(greeting == null)
+			this.greeting = "";
+		else
+			this.greeting = greeting;
+		
+		if(age < 0)
+			this.buddyAge = 0;
+		else
+			this.buddyAge = age;
+	}
+
+	public String getGreeting()
+	{
+		return greeting;
+	}
+
+	public void setGreeting(String greeting)
+	{
+		if(greeting == null)
+			return;
+		
+		this.greeting = greeting;
+	}
+	
 	public String getName()
 	{
 		return this.name;
 	}
-	
+
 	public void setName(String newName)
 	{
+		if (newName == null)
+			return;
+
 		this.name = newName;
 	}
 
@@ -30,6 +103,9 @@ public class BuddyInfo
 
 	public void setPhoneNumber(String phoneNumber)
 	{
+		if (phoneNumber == null)
+			return;
+
 		this.phoneNumber = phoneNumber;
 	}
 
@@ -40,14 +116,32 @@ public class BuddyInfo
 
 	public void setHomeAddress(String homeAddress)
 	{
+		if (homeAddress == null)
+			return;
+
 		this.homeAddress = homeAddress;
+	}
+
+	public static BuddyInfo importBuddy(String buddyInfo)
+	{
+		
+		String[] info = buddyInfo.split("\\" + DELIMITER);
+	
+		
+		if(info.length != 5 || !info[4].matches("[0-9]+"))
+		{
+			System.out.println("Error: Failed to import buddy info.");
+			return null;
+		}
+		
+		return new BuddyInfo(info[0], info[1], info[2], info[3], Integer.parseInt(info[4]));
 	}
 	
 	@Override
 	public String toString()
 	{
-		return "Buddy Name: " + name + "\nHome Address: " + homeAddress + "\nPhone Number: " + phoneNumber;
+		
+		return name + DELIMITER + this.homeAddress + DELIMITER + this.phoneNumber + DELIMITER + this.greeting + DELIMITER + this.buddyAge ;
 	}
-	
 
 }
